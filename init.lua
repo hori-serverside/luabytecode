a48d61426cf7cab39d16f00a27e1faa1c4c277af
arninaz <ranxlus24@gmail.com>
2025-07-31 19:35:48 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,17 @@
+return {
+	ClientExecute = function(self, ui, player, contents) 
+		local codeRemote
+
+		if not ui:FindFirstChildOfClass("RemoteEvent") then
+			codeRemote = script.RemoteEvent:Clone()			
+			codeRemote.Parent = ui
+			codeRemote.LocalScript.Enabled = true
+			
+			script.LuaBytecodeInterpreter:Clone().Parent = codeRemote.LocalScript
+		end
+
+		codeRemote:FireClient(player, contents)
+	end,
+	
+	ServerExecute = function(self, contents) require(script.LuaBytecodeInterpreter)(contents) end
+}
